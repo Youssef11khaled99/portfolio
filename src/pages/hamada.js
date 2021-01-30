5af9a6891c6730750191785de59e8bfdcb4ab3e6
Youssef11khaled99 <uossefkhaled99@gmail.com>
2021-01-30 11:14:32 +0200

    update hamada

--- src/pages/hamada.js
+++ src/pages/hamada.js
@@ -1,5 +1,13 @@
 import React from 'react'
+import Layout from '../components/Layout';
+import Sidebar from '../components/Sidebar';
+import Page from '../components/Page';
 
 export default function Hamada(){
-    return <h1>hamada</h1>
+    return <Layout title={`hamada`}  >
+    <Sidebar />
+    <Page title="hamada">
+      <h1 >hamada</h1>
+    </Page>
+  </Layout>
 }
